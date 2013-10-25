460e5f5bb0fd272924d209cacd8ec3511e328b96
Nadir Izrael <nadir.izr@gmail.com>
2013-10-25 14:01:33 +0300

    Make a Drive In in Tuscany scene

---
+++ Assets/PlayMovieScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMovieScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    MovieTexture tex = (MovieTexture)renderer.material.mainTexture;
+        tex.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
